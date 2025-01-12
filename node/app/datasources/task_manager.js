e70c4d773f5426a052986162d0979259b7768946
Neil Snyder <neil.r.snyder@gmail.com>
2025-01-11 16:32:14 -0800

    Add methods for querying tasks

--- node/app/datasources/task_manager.js
+++ node/app/datasources/task_manager.js
@@ -7,16 +7,54 @@ const hash_password = async (password) => {
 	return hash;
 };
 
-const get_user = async (user_data) => {
+const user_condition = async (user_data) => {
+console.log({msg: 'user condition called', user_data: user_data});
 	let params = null;
-	if(typeof user_data.id == 'number') {
-		params = {field: 'usr_id', value: user_data.id};
-	} else if (typeof user_data.username == 'string') {
-		params = {field: 'usr_username', value: user_data.username};
+
+	if(typeof user_data == 'string') {
+		user_data = {username: user_data};
+	} else if (typeof user_data == 'number') {
+		user_data = {user_id: user_data};
+	}
+console.log({user_data:user_data});
+
+	if(typeof user_data != 'object') {
+		return {res: -1, msg: "'Incorrect data type provided for user_data. Expected 'object' but got '"+(typeof user_data)+"'"};
 	}
 
-	if(!params) {
-		return {res: -1, msg: 'No username or user id provided in user_data object'};
+	if(typeof user_data.user_id == 'number') {
+		return {res: 1, field: "usr_id", value: user_data.user_id};
+	} else if (typeof user_data.username == 'string') {
+		return {res: 1, field: "usr_username", value: "'"+user_data.username+"'"};
+	}
+	
+	return {res: -1, msg: "No username or user id provided in user_data input"};
+}
+
+const task_condition = async (task_data) => {
+console.log({msg: 'task condition called', task_data: task_data});
+	if(typeof task_data == 'number') {
+		task_data = {task_id: task_data};
+	}
+console.log({task_data: task_data});
+	if(typeof task_data.task_id == 'number') {
+		return {res: 1, field: 'tsk_id', value: task_data.task_id};
+	}
+
+	const user_params = user_condition(task_data);
+console.log({user_params: user_params});
+	if(user_params.res == -1) {
+		return {res: -1, msg: "No task id provided in task_data input"};
+	}
+
+	return user_params;
+}
+
+const get_user = async (user_data) => {
+	const params = await user_condition(user_data);
+
+	if(params.res == -1) {
+		return params;
 	}
 
 	try {
@@ -62,11 +100,38 @@ const create_user = async (user_data) => {
 		let v = k == 'password' ? hash_password(user_data[k]) : user_data[k];
 		return {field: passthrough_keys[k].field, value: v};
 	});
-	
+
 	let fields = input_params.map((p) => p.field);
 	let values = input_params.map((p) => p.value);
 	await pool.execute('INSERT INTO tm_users ('+fields.join(', ')+') VALUES ('+fields.map((f) => '?').join(', ')+')', values);
 	return {res: 1, msg: 'User created'};
 };
 
-module.exports = { get_user, create_user }
+const get_tasks = async (task_data) => {
+console.log({msg: 'get tasks called', task_data: task_data});
+	const params = await task_condition(task_data);
+console.log({params: params});
+	if(params.res == -1) {
+		return params;
+	}
+
+	try {
+		const query = `SELECT tsk_id, tsk_status, tsk_owner, tsk_parent, tsk_level 
+				 FROM tasks 
+   			   INNER JOIN tm_users ON usr_id = tsk_owner 
+				WHERE ${params.field} = ?`;
+		const [recs] = await pool.execute(query, [params.value]);
+
+
+		if (recs.length == 0) {
+			return {res: 0, msg: 'No data returned'};
+		}
+console.log({msg: 'Task data retrieved', tasks: JSON.stringify(recs, null, 2)});
+		return {res: 1, tasks: recs};
+	} catch (err) {
+		console.error(err);
+		return {res: -1, msg: err};
+	}
+}
+
+module.exports = { get_user, create_user, get_tasks };
