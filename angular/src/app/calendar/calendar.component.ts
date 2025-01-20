62676bec9fab44d9a13886ace3ebdbf6adc7ccce
Neil Snyder <neil.r.snyder@gmail.com>
2025-01-19 17:06:19 -0800

    Initiate calendar component

--- angular/src/app/calendar/calendar.component.ts
+++ angular/src/app/calendar/calendar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+type CalendarView = 'Yearly' | 'Monthly' | 'Weekly' | 'Daily';
+
 @Component({
   selector: 'app-calendar',
   standalone: false,
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
 })
-export class CalendarComponent {
 
+export class CalendarComponent {
+	view: CalendarView = 'Yearly';
+	selectedDate: Date = new Date();
+
+	switchView(view: CalendarView): void { 
+		this.view = view;
+	}
+
+	navigate(direction: 'previous' | 'next'): void {
+		const date = new Date(this.selectedDate);
+		const sgn = direction === 'next' ? 1 : -1;
+		if (this.view === 'Yearly') {
+			date.setFullYear(date.getFullYear() + sgn);
+		} else if (this.view === 'Monthly') {
+			date.setMonth(date.getMonth() + sgn);
+		} else if (this.view === 'Weekl') {
+			date.setDate(date.getDate() + sgn * 7);
+		} else if (this.view === 'Daily') {
+			date.setDate(date.getDate() + sgn);
+		}
+
+		this.selectedDate = date;
+	}
 }
